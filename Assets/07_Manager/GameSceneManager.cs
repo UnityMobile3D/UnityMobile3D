52d359c08e9be1edba6e96d0692085eb62b58b05
Wanghyesung <awd5142@naver.com>
2026-01-21 20:02:53 +0900

    Test

--- Assets/07_Manager/GameSceneManager.cs
+++ Assets/07_Manager/GameSceneManager.cs
@@ -8,7 +8,6 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
-using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
 public class GameSceneManager : MonoBehaviour
 {
